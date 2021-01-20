ee0e3c715f8abb5c02b00aa1549fed7697ed0506
elio.rodriguez@ucsp.edu.pe <elio.rodriguez@ucsp.edu.pe>
2021-01-20 14:55:14 -0500

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -10,27 +10,65 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    // var newDish = Dishes({
+    //     name: 'Uthappizza',
+    //     description: 'test'
+    // });
+
+    // newDish.save()
+    //     .then((dish) => {
+    //         console.log(dish);
+
+    //         return Dishes.find({});
+    //     })
+    //     .then((dishes) => {
+    //         console.log(dishes);
+
+    //         return Dishes.remove({});
+    //     })
+    //     .then(() => {
+    //         return mongoose.connection.close();
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
+
+     Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
+    })
+    .then((dish) => {
+        //console.log(dish);
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
 
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
         });
+        console.log("update dish")
+        console.log(dish);
 
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.deleteOne({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
