932a812b15230c871511e687575587d703ec2425
elio.rodriguez@ucsp.edu.pe <elio.rodriguez@ucsp.edu.pe>
2021-01-21 18:26:20 -0500

    assigment 2

---
+++ conFusionServer/models/leaders.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// {
+//       "name": "Peter Pan",
+//       "image": "images/alberto.png",
+//       "designation": "Chief Epicurious Officer",
+//       "abbr": "CEO",
+//       "description": "Our CEO, Peter, . . .",
+//       "featured": false
+// }
+
+const leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        default: ''
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leaders;
